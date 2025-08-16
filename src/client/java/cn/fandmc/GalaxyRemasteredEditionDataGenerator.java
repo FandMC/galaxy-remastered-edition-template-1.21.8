6a7ad2374f8d9c922878c7128e9c709f3aba0c39
FandMC <winme2002@outlook.com>
2025-08-17 01:17:24 +0800

    init

---
+++ src/client/java/cn/fandmc/GalaxyRemasteredEditionDataGenerator.java
@@ -0,0 +1,11 @@
+package cn.fandmc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GalaxyRemasteredEditionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
